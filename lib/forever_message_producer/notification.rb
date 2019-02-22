7c3335393c2759a73950b7941e1e89d6ea4db3ab
Chris Ryan <cryan@forever.com>
2019-02-22 10:56:48 -0500

    Updates Notification::create To Take A Structured List Of Params

--- lib/forever_message_producer/notification.rb
+++ lib/forever_message_producer/notification.rb
@@ -3,9 +3,18 @@ module ForeverMessageProducer
   require 'forever_message_producer/workers/notification_message_worker'
 
   class Notification
-    def self.create(args)
+    def self.create(primary_type, subtype, recipient_id, body = {}, item_id = nil, expires_at = nil)
+      params = {
+        primary_type: primary_type,
+        subtype:      subtype,
+        recipient_id: recipient_id,
+        body:         body,
+        item_id:      item_id,
+        expires_at:   expires_at
+      }
+
       Sidekiq::Client.via(ForeverMessageProducer.message_bus) do
-        ForeverMessageProducer::NotificationMessageWorker.perform_async(args)
+        ForeverMessageProducer::NotificationMessageWorker.perform_async(params)
       end
     end
   end
